238a00674f9a713f8d4562b4c11307a570f2236f
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2018-10-09 21:34:24 +0300

    Don't working matrix

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -9,50 +9,12 @@
 #include "utmatrix.h"
 
 
-// Проверочный мейн для вектора
-/*void main()
-{
-	setlocale(LC_ALL, "Russian");
-	int k = 5;
-	TVector <int> a, b;
-	TVector <int> x(3), y(3);
-	cout << "Input Vectors" << endl;
-	cout << "Ввод векторов \ny= " << y << "\nx= " << x << "\na= " << a << "\nb= " << b << endl;
-	cin >> a >> b;
-	cout << "a= " << a << "|| b= " << b << "\n" << a.GetSize() << " " << a.GetStartIndex() << endl;
-	cout << "на первом месте стоит " << a[0] << endl;
-	if (x == y)
-		cout << "Равны" << endl;
-	else
-		cout << "ERROR" << endl;
-	if (a != b)
-		cout << "Не равны" << endl;
-	else
-		cout << "ERROR" << endl;
-	//a = y;
-	cout << y << endl;
-	cout << a << endl;
-
-	cout << "Операции с вектором" << endl;
-	cout << "a+b= " << a + b << endl;
-	cout << "\na-b= " << a - b << endl;
-	cout << "\na*b= " << a * b << endl;
-	cout << endl;
-	//Äåéñòâèå ñî ñêàëÿðîì
-	cout << "a+k= " << a + k << "\na-k= " << a - k << "\na*k= " << a * k << endl;
-
-	system("pause");
-}*/
-
-
 void main()
 {
   TMatrix<int> a(5), b(5), c(5);
   int i, j;
-
   setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки представления треугольных матриц"
-    << endl;
+  cout << "Тестирование программ поддержки представления треугольных матриц" << endl;
   for (i = 0; i < 5; i++)
     for (j = i; j < 5; j++ )
     {
